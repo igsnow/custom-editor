7ff27a819428c6b0df8a08dcbea401221d0d7ac3
igsnow <zzy716810918@gmail.com>
2020-01-06 23:00:17 +0800

    ä¿slate.js  例子展示å有问题

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import './App.css';
 import LikeButton from './component/count'
-import SEditor from "./component/seditor";
+import ImagesExample from "./component/seditor";
 
 function App() {
     return (
-        <div className="App">
-            <header>
-                <LikeButton/>
-                <SEditor/>
-            </header>
-        </div>
+        <>
+            <LikeButton/>
+            <ImagesExample/>
+        </>
     );
 }
 
