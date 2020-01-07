7119dababf7ab9187f3266bdb7f293d963ef23c1
igsnow <zzy716810918@gmail.com>
2020-01-07 10:54:37 +0800

    配置富文本å图片上传功能

--- src/component/beditor.js
+++ src/component/beditor.js
@@ -1,6 +1,6 @@
-import 'braft-editor/dist/index.css'
 import React, {useState} from 'react'
 import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/index.css'
 
 const BEditor = () => {
     const [state, setState] = useState({
@@ -8,11 +8,72 @@ const BEditor = () => {
     })
 
     const handleChange = (editorState) => {
+        console.log('content: ', editorState.toHTML());
         setState({editorState})
     }
 
+    // 上传
+    const myUploadFn = (param) => {
+        const serverURL = 'http://test.wvcb.qilie.biz/cgi/upload/file/item/image'
+        const xhr = new XMLHttpRequest()
+        const fd = new FormData()
+        const successFn = (response) => {
+            // 服务端返回的上传图片链接，会传入富文本编辑器
+            let resultObj = response.target.response && JSON.parse(response.target.response)
+            let uploadUrl = resultObj.data.url
+            param.success({
+                url: uploadUrl
+            })
+        }
+
+        const progressFn = (event) => {
+            // 上传进度发生变化时调用param.progress
+            param.progress(event.loaded / event.total * 100)
+        }
+
+        const errorFn = (response) => {
+            // 上传发生错误时调用param.error
+            param.error({
+                msg: 'unable to upload.'
+            })
+        }
+
+        xhr.upload.addEventListener("progress", progressFn, false)
+        xhr.addEventListener("load", successFn, false)
+        xhr.addEventListener("error", errorFn, false)
+        xhr.addEventListener("abort", errorFn, false)
+
+        fd.append('file', param.file)
+        xhr.open('POST', serverURL, true)
+        xhr.send(fd)
+
+    }
+
+    // 限制上传图片等资源文件大小
+    const myValidateFn = (file) => {
+        return file.size < 1024 * 1024 * 2
+    }
+
     return (
-        <BraftEditor value={state.editorState} onChange={handleChange}/>
+        <BraftEditor
+            value={state.editorState}
+            language={"zh"}
+            controls={[
+                'undo', 'redo', 'separator',
+                'font-size', 'line-height', 'letter-spacing', 'separator',
+                'text-color', 'bold', 'italic', 'underline', 'strike-through', 'separator',
+                'superscript', 'subscript', 'remove-styles', 'emoji', 'separator', 'text-indent', 'text-align', 'separator',
+                'headings', 'list-ul', 'list-ol', 'blockquote', 'code', 'separator',
+                'link', 'separator', 'hr', 'separator',
+                'media', 'separator',
+                'clear'
+            ]}
+            placeholder='请编辑商品详情'
+            media={{
+                uploadFn: myUploadFn,
+                validateFn: myValidateFn
+            }}
+            onChange={handleChange}/>
     )
 }
 
